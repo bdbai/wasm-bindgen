671a19ac59380d7fb6fa8554fdd677121ee78fd1
daxpedda <daxpedda@gmail.com>
2024-09-28 12:41:58 +0000

    Deploying to gh-pages from @ rustwasm/wasm-bindgen@4765448545d7bfc0223fd52f25d966b0bfbf8004 🚀

---
+++ exbuild/explicit-resource-management/pkg/explicit_resource_management.d.ts
@@ -0,0 +1,16 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+* @returns {number}
+*/
+export function current_allocation(): number;
+/**
+*/
+export class MyStruct {
+  free(): void;
+  [Symbol.dispose](): void;
+/**
+* @param {string} name
+*/
+  constructor(name: string);
+}
